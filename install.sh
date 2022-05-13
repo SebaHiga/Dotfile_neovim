c15f7e0d1d76e1745d0cfa63e7acbdfad44c6c89
Sebastian Ariel Higa <sebastianariel.higa@globant.com>
2022-05-13 12:23:26 -0300

    add installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir $HOME/.config/nvim
+INIT_FILE=$HOME/.config/nvim/init.vim
+
+if test -f ! "$INIT_FILE"; then
+    echo source $HOME/Documents/Dotfile_neovim/init.vim > $INIT_FILE
+fi
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
