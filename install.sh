d6d0440cd70e683da3c3c8ce779972b9a42b7a67
Sebastian Ariel Higa <sebastianariel.higa@globant.com>
2022-05-16 10:21:36 -0300

    totalitarian init.vim

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@
 mkdir $HOME/.config/nvim
 INIT_FILE=$HOME/.config/nvim/init.vim
 
-if [ -f "$INIT_FILE" ]; then
-	echo let g:neovim_dotfiles = $(pwd) > $INIT_FILE
-	echo source $(pwd)/init.vim >> $INIT_FILE
-fi
-
+echo -e "let g:neovim_dotfiles = '$(pwd)'\nsource $(pwd)/init.vim" > $INIT_FILE
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
